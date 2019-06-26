e955d8ccd93a5394dc27de9189becae86569bf1a
Gabriel José de Melo <gabriel.melo@ftt-20L.engcomp.local>
2019-06-26 18:12:26 -0300

    Versão 0.6

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -21,13 +21,30 @@ class UI {
 
     static addBookToList(book) {
         const list = document.querySelector('#book-list')
+
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="" class="btn-delete">X</a></td>
+        `
+
+        list.appendChild(row)
     }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 // Event: Add Book
+document.querySelector('#book-form').addEventListener('submit', e => {
+    //Get form values
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const isbn = document.querySelector('#isbn').value
+})
 
 // Event: Remove a Book
